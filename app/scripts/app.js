e81859e6461d646419d45036d042b328c5ca80c7
Marc <spam@totallyawesome.at>
2015-10-10 13:25:04 +0200

    added wallclock, brought back motion panel

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -533,3 +533,17 @@ app.controller('motionController', function($scope, $modal, munSession) {
 });
 
 
+app.controller('clockController', function($scope, $timeout) {
+    $scope.clock = "loading clock..."; // initialise the time variable
+    $scope.tickInterval = 1000 //ms
+
+    var tick = function () {
+        $scope.clock = Date.now() // get the current time
+        $timeout(tick, $scope.tickInterval); // reset the timer
+    }
+
+    // Start the timer
+    $timeout(tick, $scope.tickInterval);
+});
+
+
